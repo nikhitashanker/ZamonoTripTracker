ed97007c8716b4b40a315616aafa55367d4c6b3b
nikhitashanker <rocketns@gmail.com>
2017-06-11 12:26:45 -0700

    Hello World basic application

---
+++ app/src/main/java/nikhi/zamono/MainActivity.java
@@ -0,0 +1,13 @@
+package nikhi.zamono;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
